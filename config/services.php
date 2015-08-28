4173aea6e9bc37bcc67989efe86d6a3f0086892e
Bijay Gurung <bjgurung10@gmail.com>
2015-08-28 08:36:49 +0545

    Before domain change in session.php

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'facebook' => [
             'client_id' => env('FACEBOOK_CLIENT_ID'),
             'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-            'redirect' => 'http://localhost:8000/login/facebook',
+            'redirect' => 'http://plpredict/login/facebook',
     ],
 
 ];
