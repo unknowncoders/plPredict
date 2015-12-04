a3fe7174559cf08e77d44853bedd2e5beb0156b0
idea <idea@idea.idea>
2015-12-04 08:59:54 +0545

    css of home page is modified

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,32 +10,28 @@
 
 <div class ="col-xs-12 col-sm-8 col-lg-9 col-md-9 thumbnail gameweekbox">
 
-
-//this create the problem when there is only the last gameweek;
- <ul class ="nav nav-tabs">
-        <li class="active"><a data-toggle="tab" href="#predictions" class="showtabs">Gameweek {{ $gws[0]->id }}</a></li>
-        <li><a data-toggle="tab" href="#badges" class="showtabs">Gameweek {{ $gws[1]->id }}</a></li>
- </ul>
-
-</div>
-
-<!--
-    @foreach ($gws as $gw)
-        
-        <div class="col-xs-12  col-sm-8 col-md-9 col-lg-9 predictinfo ">
+<ul class="nav nav-tabs">
+        <li class="active"><a data-toggle="tab" href="#current" class="showtabs"> Gameweek {{ $gws[0]->id }}</a></li>
+    @if(count($gws)===2)    
+     <li><a data-toggle="tab" href="#next" class="showtabs"> Gameweek {{ $gws[1]->id }}</a></li>
+      @endif 
+</ul>
+ 
+    <div class="tab-content">
+       
+        <div id="current" class="tab-pane active">
 
 
-           <strong>  <h1 class="colorwhite">Gameweek {{ $gw->id }}  </h1> </strong>
-            <hr>
-                <br>
+           <br><br>
+
                     {!! Form::open(['url'=>'predictions']) !!}
                                         
-                        @foreach ($gw->fixtures as $fxt)
-                             <div class="col-md-12 col-sm-12 col-lg-5 thumbnail predictbox "> 
+                        @foreach ($gws[0]->fixtures as $fxt)
+                             <div class="col-md-12 col-sm-12 col-lg-5 thumbnail gameweekpredictbox "> 
                                     @if ($fxt->isClosed())
 
-                                        @if ($gw->boostId == $fxt->id) 
-                                            <span> Boosted!! </span>
+                                        @if ($gws[0]->boostId == $fxt->id) 
+                                            <span class="colorred"> Boosted!! </span>
                                         @endif
                                     
                                       <span class="badge predictscore">
@@ -107,32 +103,149 @@
 
                                 </div>
                             @endforeach
-                            <div class="col-sm-12">
+    
+                                <div class="col-xs-12" >
+                                        @if(!$gws[0]->boostedClosed)
 
-                                <div class="col-xs-9">
-                                        @if(!$gw->boostedClosed)
-                                            <div style="font-size: 1.1em; text-align: right;" class="col-xs-4 colorwhite">
-                                                    <label for="boost_id">Boost:</label>
+                                            <div  class="col-xs-1" class="boost">
+                                                    <label for="boost_id" >Boost</label>
                                             </div>
-                                            <div class="col-xs-8">
-                                                    <select class="form-control" id="boost_id" name="boost_id">
-                                                        @foreach($gw->fixtures()->open()->get() as $cfxt)
-                                                        <option value="{{ $cfxt->id }}" <?php if($gw->boostId == $cfxt->id){echo "selected";}?>>
+                                            <div class="col-xs-9" class="boostfield">
+                                                    <select class="form-control boostfield" id="boost_id" name="boost_id">
+                                                        @foreach($gws[0]->fixtures()->open()->get() as $cfxt)
+                                                        <option value="{{ $cfxt->id }}" <?php if($gws[0]->boostId == $cfxt->id){echo "selected";}?>>
                                                                 {!!$cfxt->homeClub->name!!} &nbsp vs &nbsp {!!$cfxt->awayClub->name!!}
                                                         </option>
                                                         @endforeach
                                                     </select>
                                             </div>
-                                        @endif
-                                </div>
-                                <div class="col-xs-3">
+                                @endif
+                                
+                                 <div class="col-xs-1 predictsave">
                                         {!! Form::submit('Save',['class'=>'predictsubmit']) !!}
+                                 </div>
+                                
                                 </div>
+                                
                                 <br>               
-                            </div>
                     {!! Form::close() !!}
-        </div>
-        @endforeach
+        </div> 
 
+      <div id="next" class="tab-pane fade">
+
+<br><br>
+            @if(count($gws)===2)
+
+                    {!! Form::open(['url'=>'predictions']) !!}
+                                        
+                        @foreach ($gws[1]->fixtures as $fxt)
+                             <div class="col-md-12 col-sm-12 col-lg-5 thumbnail gameweekpredictbox "> 
+                                    @if ($fxt->isClosed())
+
+                                        @if ($gws[1]->boostId == $fxt->id) 
+                                            <span class="colorred"> Boosted!! </span>
+                                        @endif
+                                    
+                                      <span class="badge predictscore">
+                                        @if ($fxt->isOver())
+                                            @if ($fxt->predictions->count()!=0)
+                                                {{ $fxt->predictions[0]->score() }}
+                                            @else
+                                                -
+                                                @endif
+                                        @else
+                                            -
+                                        @endif
+                                     </span>
+                                    <br><br>
+                                     <p>  {{ $fxt->homeClub->name }} </p>
+                                    
+                                        
+                                         <p class="predictdata"> 
+
+                                         @if ($fxt->predictions->count() != 0)
+                                                 {{ $fxt->predictions[0]->home_score }}
+                                             @else
+                                               - 
+                                              @endif
+                                       </p>
+         
+                                       <p class="predictresult">
+
+                                       @if ($fxt->isOver())
+                                            {{ $fxt->home_score }} 
+                                       @else
+                                            -
+                                         @endif
+                                      </p>
+                                          <br><br> 
+                                     <p class="predictdata">
+                                        @if ($fxt->predictions->count() != 0)
+                                            {{ $fxt->predictions[0]->away_score }}                                
+                                        @else
+                                              - 
+                                        @endif
+                                  
+                                     </p>
+                                    
+                                     <p class="predictresult">
+                                     @if ($fxt->isOver())
+                                            {{ $fxt->away_score }}
+                                            
+                                             @else
+                                            -
+                                        @endif
+                              
+                                     </p> 
+                                    <p class="predictname">  {{ $fxt->awayClub->name }} </p>
+
+                                    @else
+                                        <?php if( $fxt->predictions->count()==0){
+                                                  $p_home_score = 0;
+                                                  $p_away_score = 0;
+                                                  $hasPrediction = false;
+                                                }else{
+                                                  $p_home_score = $fxt->predictions[0]->home_score;
+                                                  $p_away_score = $fxt->predictions[0]->away_score;
+                                                  $hasPrediction = true; }
+                                        ?>
+
+                                            @include ('partials.predict_form',['hasPrediction'=>$hasPrediction,'p_home_score'=>$p_home_score,'p_away_score'=>$p_away_score])
+                                    @endif
+
+                                </div>
+                            @endforeach
+    
+                                <div class="col-xs-12" >
+                                        @if(!$gws[1]->boostedClosed)
+
+                                            <div  class="col-xs-1" class="boost">
+                                                    <label for="boost_id" >Boost</label>
+                                            </div>
+                                            <div class="col-xs-9" class="boostfield">
+                                                    <select class="form-control boostfield" id="boost_id" name="boost_id">
+                                                        @foreach($gws[1]->fixtures()->open()->get() as $cfxt)
+                                                        <option value="{{ $cfxt->id }}" <?php if($gws[1]->boostId == $cfxt->id){echo "selected";}?>>
+                                                                {!!$cfxt->homeClub->name!!} &nbsp vs &nbsp {!!$cfxt->awayClub->name!!}
+                                                        </option>
+                                                        @endforeach
+                                                    </select>
+                                            </div>
+                                @endif
+                                
+                                 <div class="col-xs-1 predictsave">
+                                        {!! Form::submit('Save',['class'=>'predictsubmit']) !!}
+                                 </div>
+                                
+                                </div>
+                                
+                                <br>               
+                    {!! Form::close() !!}
+             
+            @endif 
+       </div>
+
+   </div> 
+ 
+</div>
 @stop
--->
